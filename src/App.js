34e44991cf048de58b5ffc7872d8392d6d706a9b
irsayvid <dsdarimisetti@gmail.com>
2022-10-11 20:09:02 +0530

    fetch weather data using coordinates

--- src/App.js
+++ src/App.js
@@ -7,48 +7,39 @@ function App() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [city, setCity] = useState("New York City")
   const [results, setResults] = useState(null);
-  // "http://api.openweathermap.org/geo/1.0/direct?"+lat+"&"+lon+"&appid={API key}
-  // const[lat,setLat]=useState(null);
-  // const[lon,setLon]=useState(null);
-  var lat,lon;
-  async function success(position) {
-    lat=position.coords.latitude;
-    console.log(lat);
-    // setLat(position.coords.latitude);
-    // setLon(position.coords.longitude);
-    // console.log(lat);
-    lon=position.coords.longitude;
-    console.log(lon);
+  const[coordinates,setCoordinates]=useState({lat:40.7128,lon:-74.0060});
+  function success(position) {
+    setCoordinates({lat:position.coords.latitude, lon:position.coords.longitude});
   }
+  
+  useEffect(() => {
+    if (navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(success);
+    }
+    else{
+      alert("Geolocation is not supported by this browser.");
+    }
+  }, [])
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(success);
-    fetch("http://api.openweathermap.org/geo/1.0/reverse?lat="+lat+"&lon="+lon+"&limit=5&appid=" + process.env.REACT_APP_APIKEY)
-    .then(res=>res.json())
+    fetch("http://api.openweathermap.org/data/2.5/weather?lat="+coordinates.lat+"&lon="+coordinates.lon+"&limit=5&appid=" + process.env.REACT_APP_APIKEY)
+    .then(res => res.json())
     .then(
-      (response)=>{
-    //     setLat(response["coord"]["lat"]);
-    //     setLon(response.coord.lon);
-        console.log(response[0].name);
+      (result) => {
+        if (result['cod'] !== 200) {
+          setIsLoaded(false);
+        } else {
+          setIsLoaded(true);
+          setResults(result);
+          setCity(result.name + ", " + result.sys.country);
+        }
+      },
+      (error) => {
+        setIsLoaded(true);
+        setError(error);
       }
     )
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&appid=" + process.env.REACT_APP_APIKEY)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          if (result['cod'] !== 200) {
-            setIsLoaded(false)
-          } else {
-            setIsLoaded(true);
-            setResults(result);
-          }
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      )
-  }, [city])
+  }, [coordinates])
 
   if (error) {
     return <div>Error: {error.message}</div>;
@@ -63,7 +54,7 @@ function App() {
           onChange={event => setCity(event.target.value)} />
         <div className="Results">
           {!isLoaded && <h2>Loading...</h2>}
-          {/* {console.log(results)} */}
+          {console.log(results)}
           {isLoaded && results && <>
             <h3>{results.weather[0].main}</h3>
             <p>Feels like {results.main.feels_like}°C</p>
