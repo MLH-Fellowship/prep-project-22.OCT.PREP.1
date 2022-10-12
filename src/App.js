a64c3dfb87d498b5f2b6baf3383e80999573197c
SudhanshuBlaze <skrourkela@gmail.com>
2022-10-12 20:31:46 +0530

    Merge branch 'main' of https://github.com/SudhanshuBlaze/prep-project-22.OCT.PREP.1 into main

--- src/App.js
+++ src/App.js
@@ -1,110 +1,131 @@
 import { useEffect, useState } from "react";
-import './App.css';
-import logo from './mlh-prep.png'
+import "./App.css";
+import logo from "./mlh-prep.png";
 import MapBox from "./components/Map/MapBox";
+import Forecast from "./components/Map/Forecast";
 
 function App() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [city, setCity] = useState("New York City")
+  const [city, setCity] = useState("New York City");
   const [results, setResults] = useState(null);
   const [coordinates, setCoordinates] = useState({
     lat: 40.7143,
-    lon: -74.006
+    lon: -74.006,
   });
-  const [weatherType, setWeatherType] = useState("")
+  const [weatherType, setWeatherType] = useState("");
 
-
-  const findUserLocation = (position) => {
-    const latitude = position.coords.latitude, longitude = position.coords.longitude;
-    fetch("https://api.openweathermap.org/geo/1.0/reverse?lat="+latitude+"&lon="+longitude+"&limit=5&appid=" + process.env.REACT_APP_APIKEY)
-    .then(res => res.json())
-    .then(
-      (result) => {
-        console.log(result);
-          setCity(result[0].name);
-        }
+  const findUserLocation = position => {
+    const latitude = position.coords.latitude,
+      longitude = position.coords.longitude;
+    fetch(
+      "https://api.openweathermap.org/geo/1.0/reverse?lat=" +
+        latitude +
+        "&lon=" +
+        longitude +
+        "&limit=5&appid=" +
+        process.env.REACT_APP_APIKEY
     )
-  }
-  
+      .then(res => res.json())
+      .then(result => {
+        console.log(result);
+        setCity(result[0].name);
+      });
+  };
+
   useEffect(() => {
-    if (navigator.geolocation){
+    if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(findUserLocation);
-    }
-    else{
+    } else {
       alert("Geolocation is not supported by this browser.");
     }
-  }, [])
+  }, []);
 
   useEffect(() => {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + process.env.REACT_APP_APIKEY)
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        process.env.REACT_APP_APIKEY
+    )
       .then(res => res.json())
       .then(
-        (result) => {
-          if (result['cod'] !== 200) {
-            setIsLoaded(false)
+        result => {
+          if (result["cod"] !== 200) {
+            setIsLoaded(false);
           } else {
             setIsLoaded(true);
-            console.log(result)
+            console.log(result);
             setResults(result);
             setCoordinates(result.coord);
             setWeatherType(result.weather[0].main);
           }
         },
-        (error) => {
+        error => {
           setIsLoaded(true);
           setError(error);
           setWeatherType(error);
-        })
-      }, [city])
+        }
+      );
+  }, [city]);
 
-  const weather = (weatherType) => {
+  const weather = weatherType => {
     switch (weatherType) {
       case "Clouds":
-        return "cloudy"
+        return "cloudy";
       case "Clear":
-        return "clear"
+        return "clear";
       case "Rain":
-        return "rainy"
+        return "rainy";
       case "Snow":
-        return "snowy"
+        return "snowy";
       case "Thunderstorm":
-        return "stormy"
+        return "stormy";
       case "Drizzle":
-        return "drizzly"
+        return "drizzly";
       default:
-        return "haze"
+        return "haze";
     }
-  }
+  };
 
   if (error) {
     return <div>Error: {error.message}</div>;
   } else {
-    return <div className={"main " + weather(weatherType)}>
-      <img className="logo" src={logo} alt="MLH Prep Logo"></img>
-      <div>
-        <h2>Enter a city below 👇 or Click on a location in 🗺</h2>
-        <input
-          type="text"
-          value={city}
-          onChange={event => setCity(event.target.value)} />
-        <MapBox 
-          coordinates={coordinates} 
-          setCoordinates={setCoordinates} 
-          setResults={setResults}
-          setError={setError}
-          setCity={setCity}
-         />
-        <div className="Results">
-          {!isLoaded && <h2>Loading...</h2>}
-          {isLoaded && results && <>
-            <h3>{results.weather[0].main}</h3>
-            <p>Feels like {results.main.feels_like}°C</p>
-            <i><p>{results.name}, {results.sys.country}</p></i>
-          </>}
+    return (
+      <div className={"main " + weather(weatherType)}>
+        <img className="logo" src={logo} alt="MLH Prep Logo"></img>
+        <div>
+          <h2>Enter a city below 👇 or Click on a location in 🗺</h2>
+          <input
+            type="text"
+            value={city}
+            onChange={event => setCity(event.target.value)}
+          />
+          <MapBox
+            coordinates={coordinates}
+            setCoordinates={setCoordinates}
+            setResults={setResults}
+            setError={setError}
+            setCity={setCity}
+          />
+          <div className="Results">
+            {!isLoaded && <h2>Loading...</h2>}
+            {isLoaded && results && (
+              <>
+                <h3>{results.weather[0].main}</h3>
+                <p>Feels like {results.main.feels_like}°C</p>
+                <i>
+                  <p>
+                    {results.name}, {results.sys.country}
+                  </p>
+                </i>
+              </>
+            )}
+          </div>
         </div>
+        <Forecast />
       </div>
-    </div>
+    );
   }
 }
 
---
+++ src/components/Map/Forecast.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+export default function Forecast({ city }) {
+  const [forecastData, setForecastData] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${process.env.REACT_APP_APIKEY}`
+    )
+      .then(res => res.json())
+      .then(
+        result => {
+          if (result["cod"] == 200) {
+            setForecastData(result);
+            console.log("forecast", result);
+          }
+        },
+        error => {
+          console.log(error);
+        }
+      );
+  }, [city]);
+  return <div>Forecast</div>;
+}
