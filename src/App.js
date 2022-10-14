005326cf7d60e2e952e81b9bfac1fcd7780a95d7
irsayvid <dsdarimisetti@gmail.com>
2022-10-14 22:32:24 +0530

    fix deploy issue

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 import './App.css';
 import logo from './mlh-prep.png'
 
-import ItemsNeeded from "./Components/ItemsNeeded";
-import MapBox from "./Components/Map/MapBox";
+import ItemsNeeded from "./components/ItemsNeeded";
+import MapBox from "./components/Map/MapBox";
 
 
 function App() {
