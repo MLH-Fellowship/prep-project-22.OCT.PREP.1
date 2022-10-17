669a8dd418120e93c289d3c3369721f6ac035bdc
Niyoj Oli <oliniyoj@gmail.com>
2022-10-17 16:43:31 +0545

    adds custom markers

--- src/components/Map/LocationMarker.js
+++ src/components/Map/LocationMarker.js
@@ -1,52 +1,65 @@
-import L from 'leaflet';
+import L from "leaflet";
 import { useEffect, useState } from "react";
 import "leaflet/dist/leaflet.css";
-import { useMap, Marker, Popup, useMapEvent } from 'react-leaflet'
+import { useMap, Marker, Popup, useMapEvent } from "react-leaflet";
 
-export default function LocationMarker({coordinates, setCoordinates, setResults, setError, setCity}) {
-    const [weatherIcon, setWeatherIcon] = useState('');
+export default function LocationMarker({
+    coordinates,
+    setCoordinates,
+    setResults,
+    setError,
+    setCity,
+}) {
+    const [weatherIcon, setWeatherIcon] = useState("10d");
 
     const greenIcon = new L.Icon({
-        iconUrl:"http://openweathermap.org/img/w/" + weatherIcon + ".png",
+        iconUrl: "http://openweathermap.org/img/w/" + weatherIcon + ".png",
         iconSize: [35, 41],
         iconAnchor: [12, 41],
         popupAnchor: [1, -34],
         shadowSize: [41, 41],
     });
-    
+
     const [position, setPosition] = useState({
         lat: 40.7143,
-        lon: -74.006
+        lon: -74.006,
     });
 
-    useEffect(()=>{
-        if(position != null){
-            fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + position.lat + "&lon=" + position.lon + "&units=metric&appid=" + process.env.REACT_APP_APIKEY)
-            .then(res => res.json())
-            .then((res)=>{
-                if (res['cod'] !== 200) {
-                } else {
-                    setResults(res);
-                    setCity(res.name);
-                    setWeatherIcon(res.weather[0].icon);
-                }
-            })
-            .catch((error)=>{
-                setError(error);
-            });
+    useEffect(() => {
+        if (position != null) {
+            fetch(
+                "https://api.openweathermap.org/data/2.5/weather?lat=" +
+                    position.lat +
+                    "&lon=" +
+                    position.lon +
+                    "&units=metric&appid=" +
+                    process.env.REACT_APP_APIKEY
+            )
+                .then((res) => res.json())
+                .then((res) => {
+                    if (res["cod"] !== 200) {
+                    } else {
+                        setResults(res);
+                        setCity(res.name);
+                        setWeatherIcon(res.weather[0].icon);
+                    }
+                })
+                .catch((error) => {
+                    setError(error);
+                });
         }
-    }, [position, weatherIcon])
+    }, [position]);
 
     const map = useMap();
     map.setView(coordinates);
-    useMapEvent('click', (e)=>{
+    useMapEvent("click", (e) => {
         setCoordinates({
             lat: e.latlng.lat,
-            lon: e.latlng.lng
+            lon: e.latlng.lng,
         });
         setPosition({
             lat: e.latlng.lat,
-            lon: e.latlng.lng
+            lon: e.latlng.lng,
         });
     });
 
