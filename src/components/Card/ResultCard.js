8f7d07f2eef64957b6b52d73409ada920f8c34d1
bhambrilavisha <bhambrilavisha@gmail.com>
2022-10-20 22:26:22 +0530

    display data in result card

--- src/components/Card/ResultCard.js
+++ src/components/Card/ResultCard.js
@@ -1,26 +1,45 @@
 import "../../App.css";
 import ItemsNeeded from "../CarryItems/ItemsNeeded";
-const ResultCard=({results,isLoaded,error})=>{
+
+const ResultCard=({results,isLoaded,error,airPollutionResults})=>{
    return <div className="Results">
     {!isLoaded && error && (
         <h3 style={{ color: "red" }}>{error.message}</h3>
     )}
     {isLoaded && results && (
         <>
-        <img
-            src={
-            "http://openweathermap.org/img/w/" +results.weather[0].icon +".png"} alt="Weather icon"
-        />
-        <h3>{results.weather[0].main}</h3>
-        <p>{results.weather[0].description}</p>
-        <p>Feels like {results.main.feels_like}°C</p>
-        <p>Humidity {results.main.humidity}%</p>
-        <i>
-            <p>
-            {results.name}, {results.sys.country}
-            </p>
-        </i>
-        <ItemsNeeded weatherKind={results.weather[0].main} />
+        <div>
+            <img
+                src={
+                "http://openweathermap.org/img/w/" +results.weather[0].icon +".png"} alt="Weather icon"
+            />
+            <h3>{results.weather[0].main}</h3>
+            <p>{results.weather[0].description}</p>
+            <p>Feels like {results.main.feels_like}°C</p>
+            <p>Humidity {results.main.humidity}%</p>
+            <i>
+                <p>
+                {results.name}, {results.sys.country}
+                </p>
+            </i>
+        </div>
+        
+        <div>
+            <ItemsNeeded weatherKind={results.weather[0].main} />
+        </div>
+
+        <div>
+            <h3>Air Pollution Details</h3>
+            <p> CO: {airPollutionResults.co}</p>
+            <p> NO: {airPollutionResults.no}</p>
+            <p> NO2: {airPollutionResults.no2}</p>
+            <p> O3: {airPollutionResults.o3}</p>
+            <p> SO2: {airPollutionResults.so2}</p>
+            <p> PM2_5: {airPollutionResults.pm2_5}</p>
+            <p> PM10: {airPollutionResults.pm10}</p>
+            <p> NH3: {airPollutionResults.nh3}</p>
+        </div>
+        
         </>
         )}
     </div> 
