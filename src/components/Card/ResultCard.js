9ce7b80a5b580857809e5097169f810bb6fca97c
rk119 <rk119@hw.ac.uk>
2022-10-21 14:09:28 +0400

    Enhance Result Card UI

--- src/components/Card/ResultCard.js
+++ src/components/Card/ResultCard.js
@@ -1,40 +1,67 @@
-import "../../App.css";
+import "../../assets/styles/ResultCard.css";
 import ItemsNeeded from "../CarryItems/ItemsNeeded";
-const ResultCard=({results,isLoaded,error})=>{
+import {
+    WiThunderstorm,
+    WiShowers,
+    WiRain,
+    WiDayHaze,
+    WiSnow,
+    WiSmoke,
+    WiDust,
+    WiFog,
+    WiSandstorm,
+    WiSmog,
+    WiRainWind,
+    WiTornado,
+    WiDaySunny,
+    WiCloudy
+  } from "weather-icons-react"
+
+const ResultCard=({results, isLoaded, error})=>{
+    const icon = x => {
+        if (x === "Rain") return <WiRain />;
+        if (x === "Snow") return <WiSnow />;
+        if (x === "Clear") return <WiDaySunny size={80}/>;
+        if (x === "Clouds") return <WiCloudy />;
+        if (x === "Tornado") return <WiTornado />;
+        if (x === "Drizzle") return <WiShowers />;
+        if (x === "Thunderstorm") return <WiThunderstorm />;
+        if (x === "Mist") return <WiDayHaze />;
+        if (x === "Squall") return <WiRainWind />;
+        if (x === "Dust") return <WiDust />;
+        if (x === "Ash") return <WiSmog />;
+        if (x === "Sand") return <WiSandstorm />;
+        if (x === "Fog") return <WiFog />;
+        if (x === "Smoke") return <WiSmoke />;
+        if (x === "Haze") return <WiDayHaze />;
+        else return <WiDaySunny />;
+    };
+
    return <div className="Results">
     {!isLoaded && error && (
         <h3 style={{ color: "red" }}>{error.message}</h3>
     )}
     {isLoaded && results && (
         <>
-        <div className="results-content">
-            <div className="top-grid">
-                <div>
-                    <i><p>{results.name}, {results.sys.country}</p></i>
-                    <span className="grid-item-text">
-                        {Math.round(results.main.temp)}°C
-                    </span>
+        <div className="result-content">
+            <div className="grid">
+                <div className="grid-item-one">
+                    <div className="text">
+                        <span className="icon">{icon(results.weather[0].main)}</span>
+                        <span>{Math.round(results.main.temp)}°C</span>
+                    </div>
+                    <span><strong>{results.weather[0].main}</strong></span>
                 </div>
-                <div className="bottom-grid">
-                    <span>{results.weather[0].main}
-                    <img
-                        src={
-                        "http://openweathermap.org/img/w/" +results.weather[0].icon +".png"} alt="Weather icon"
-                    /></span>
-                    <span>Feels like: {Math.round(results.main.feels_like)}°C</span>
-                    <span>Humidity: {results.main.humidity}%</span>
+                <div className="grid-item-two">
+                    <i className="location">{results.name}, {results.sys.country}</i>
+                    <span><strong>FEELS LIKE: </strong>{Math.round(results.main.feels_like)}°C</span>
+                    <span><strong>HUMIDITY: </strong>{Math.round(results.main.humidity)}%</span>
                 </div>
-                {/* <div className="grid-item"> */}
-                {/* </div> */}
             </div>
-            {/* <div className="bottom-grid"> */}
-                {/* <span>Feels like: {Math.round(results.main.feels_like)}°C</span> */}
-                {/* <span>Humidity: {results.main.humidity}%</span> */}
-            {/* </div> */}
         </div>
-        {/* <div className="results-items">
+        <div className="result-items">
             <ItemsNeeded weatherKind={results.weather[0].main} />
-        </div> */}
+        </div>
         </>
         )}
     </div>  
