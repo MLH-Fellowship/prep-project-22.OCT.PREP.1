824dc359ffb99bb301c2bad0d41c989f6ef3bd01
Niyoj Oli <oliniyoj@gmail.com>
2022-10-21 19:42:21 +0545

    removes brief detail of atmosphere component temporary

--- src/components/Card/ResultCard.js
+++ src/components/Card/ResultCard.js
@@ -2,7 +2,7 @@ import "../../App.css";
 import ItemsNeeded from "../CarryItems/ItemsNeeded";
 import "./ResultCard.css";
 
-const ResultCard=({results,isLoaded,error,airPollutionResults,aqi})=>{
+const ResultCard=({results,isLoaded,error,aqi})=>{
     let aqiWord = "Very Good";
     console.log("aqi: "+aqi);
     if (parseInt(aqi) === 1) aqiWord = "Good";
@@ -34,18 +34,6 @@ const ResultCard=({results,isLoaded,error,airPollutionResults,aqi})=>{
         
         <div>
             <ItemsNeeded weatherKind={results.weather[0].main} />
-        </div>
-        
-        <div className="container">
-            <h3>Air Pollution Details</h3>
-            <p> CO: {airPollutionResults.co}</p>
-            <p> NO: {airPollutionResults.no}</p>
-            <p> NO2: {airPollutionResults.no2}</p>
-            <p> O3: {airPollutionResults.o3}</p>
-            <p> SO2: {airPollutionResults.so2}</p>
-            <p> PM2_5: {airPollutionResults.pm2_5}</p>
-            <p> PM10: {airPollutionResults.pm10}</p>
-            <p> NH3: {airPollutionResults.nh3}</p>
         </div>
 
         <div className="container">
